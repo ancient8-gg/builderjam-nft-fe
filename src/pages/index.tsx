42752672129083bb7083183c1099bb64f0f5b2fb
Richard <thanhle@ancient8.gg>
2024-12-13 17:30:55 +0700

    feat: add ABI & read from contract

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
 import { useState } from 'react';
 import type { NextPage } from 'next';
+import { useAccount, useReadContract } from 'wagmi';
+import { Address } from 'viem';
 
 import Layout from '../components/Layout';
 import NftViewer from '../components/NftViewer';
 import Minter from '../components/Minter';
 
+import { BUILDER_JAM_NFT_ABI } from '../abi/builder-jam-nft.abi';
+
 const Home: NextPage = () => {
-  const [tokenIds, setTokenIds] = useState<string[]>([]); /* Push your tokenIds here */
-  const metadataUri = undefined; /* Mock, push your metadataUri here */
+  const { isConnected } = useAccount();
+
+  const [tokenIds, setTokenIds] = useState<string[]>(['1']);
+
+  const { data: tokenURI } = useReadContract({
+    address: process.env.NEXT_PUBLIC_COLLECTION_ADDRESS as Address,
+    abi: BUILDER_JAM_NFT_ABI,
+    functionName: 'tokenURI',
+    args: [0],
+  });
 
   return (
     <Layout>
@@ -16,10 +28,10 @@ const Home: NextPage = () => {
           key={tokenId}
           address={process.env.NEXT_PUBLIC_COLLECTION_ADDRESS}
           tokenId={tokenId}
-          metadataUri={metadataUri}
+          metadataUri={tokenURI as string}
         />
       ))}
-      <Minter onMint={() => {} /*Fill your mint func here*/} />
+      <Minter onMint={() => {}} disabled={!isConnected} />
     </Layout>
   );
 };
